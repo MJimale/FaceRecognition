ff63d42f81c65204f73639cb014eb9588e9daa2f
mustafa hussein <mustafa1996.mj@gmail.com>
2018-11-21 04:43:52 +0000

    Updates

---
+++ precache-manifest.c707b0f75900a09b0b259bf56eafd1a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ef5801f2aec18bace19",
+    "url": "/FaceRecognition/static/js/runtime~main.4ef5801f.js"
+  },
+  {
+    "revision": "66903894651344ae8c79",
+    "url": "/FaceRecognition/static/js/main.66903894.chunk.js"
+  },
+  {
+    "revision": "f1febbcc218ecac179f3",
+    "url": "/FaceRecognition/static/js/1.f1febbcc.chunk.js"
+  },
+  {
+    "revision": "66903894651344ae8c79",
+    "url": "/FaceRecognition/static/css/main.e20311a8.chunk.css"
+  },
+  {
+    "revision": "f1febbcc218ecac179f3",
+    "url": "/FaceRecognition/static/css/1.9e1c963b.chunk.css"
+  },
+  {
+    "revision": "8e2da39a99ed184bd35685ef666ff088",
+    "url": "/FaceRecognition/index.html"
+  }
+];
